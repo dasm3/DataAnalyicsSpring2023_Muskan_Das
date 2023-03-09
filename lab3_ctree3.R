ab27719634040e52cb1a95fc6deb3f8c18e147b8
dasm3 <119474037+dasm3@users.noreply.github.com>
2023-03-09 14:54:58 -0500

    Add files via upload

---
+++ lab3_ctree3.R
@@ -0,0 +1,8 @@
+#Ctree3_Lab3
+
+fitK <- ctree(Kyphosis ~ Age + Number + Start, data=kyphosis)
+plot(fitK, main="Conditional Inference Tree for Kyphosis”)
+plot(fitK, main="Conditional Inference Tree for Kyphosis",type="simple")
+
+#etc.
+
